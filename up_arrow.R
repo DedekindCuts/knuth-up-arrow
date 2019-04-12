68019ff1d790ba1a4b052695bf1ac25107e711e7
Nick Larsen <n.larsen@veracityfa.com>
2019-04-12 11:28:01 -0600

    Require a to be a nonnegative integer; suppress package startup messages

--- up_arrow.R
+++ up_arrow.R
@@ -1,28 +1,29 @@
-library(bit64)
+suppressPackageStartupMessages(library(bit64))
 
-up_arrow <- function(a, n.arrows, b){
+up_arrow <- function(a, n, b){
 
 	#check for good input
-	if(!is.numeric(a)){
-		stop("a must be a number")
+	if(a < 0 | (a - as.integer64(a) != 0)){
+		stop("a must be a nonnegative integer")
 	}
-	if(n.arrows < 0 | (n.arrows - as.integer64(n.arrows) != 0)){
+	if(n < 0 | (n - as.integer64(n) != 0)){
 		stop("Number of arrows must be a nonnegative integer")
 	}
 	if(b < 0 | (b - as.integer64(b) != 0)){
 		stop("b must be a nonnegative integer")
 	}
 
+	# store inputs as 64-bit integers
 	a.big <- as.integer64(a)
-	n.arrows.big <- as.integer64(n.arrows)
+	n.big <- as.integer64(n)
 	b.big <- as.integer64(b)
 
 	#evaluate
-	if(n.arrows.big == 0){
+	if(n.big == 0){
 		return(a.big*b.big)
 	} else if(b.big == 0){
 		return(1)
 	} else {
-		return(up_arrow(a.big, n.arrows.big - 1, up_arrow(a.big, n.arrows.big, b.big-1)))
+		return(up_arrow(a.big, n.big - 1, up_arrow(a.big, n.big, b.big-1)))
 	}
 }
